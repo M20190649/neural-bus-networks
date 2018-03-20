e3e52e6d3fa6f99fdc59b98282d1b3d4ddf59b2e
cooper-sloan <cooper@isloan.net>
2018-03-20 12:56:34 -0400

    Updates

--- python/analyze_all_named_stops.py
+++ python/analyze_all_named_stops.py
@@ -27,28 +27,25 @@ def unixTimeSeconds(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
     return (dt - epoch).total_seconds()
 
-if __name__ == '__main__':
+def processData(h5filenames):
     if len(sys.argv) <= 1:
         print 'No route specified, assuming Route 1'
         theroute = 1
     else:
         theroute = int(sys.argv[1])
         print 'Route to analyze: %d' % theroute
-
     if len(sys.argv) <= 2:
         print 'No direction specified, assuming I (inbound)1'
         thedirection = 'I' #'O'
     else:
         thedirection = sys.argv[2]
         print 'Direction to analyze: %s' % thedirection
-
-    if len(sys.argv) <= 3:
-        print 'No input filename specified: will scan for all *.h5 files'
-        h5filenames = glob('/Volumes/Infinity/mbta/h5/2013/*.h5')
-        print(h5filenames)
-    else:
-        h5filenames = [sys.argv[3]]
-
+    # if len(sys.argv) <= 3:
+        # print 'No input filename specified: will scan for all *.h5 files'
+        # h5filenames = glob('/Volumes/Infinity/mbta/h5/2013/*.h5')
+        # print(h5filenames)
+    # else:
+        # h5filenames = [sys.argv[3]]
     if len(sys.argv) <= 4:
         print 'No output filename specified: saving to spacings.mat'
         outfilename = 'spacings'
@@ -94,136 +91,82 @@ if __name__ == '__main__':
 
         #Iterate over all timed bus stops
         for schedule_code in ['W', 'S', 'U']:
-            # named_bus_stops has name and times to hit that stop
-            # named_bus_stops = ReadMBTATimetable(route = theroute, direction = thedirection,
-                    # timing = schedule_code)
-
-            #Now iterate over ALL bus stops
             # all_bus_stops has just name and location
             for stop_idx, (this_bus_stop_location, this_bus_stop_name) in enumerate(all_bus_stops):
                 print("File: " +str(file_index) + "/" + str(len(h5filename))+"."+ "Processing stop: "+str(stop_idx)+"/"+str(len(all_bus_stops)))
-
-                #See if this is a bus stop that has a timetable on the MBTA website
-                # timetable = None
-                # timetable_chunks = None
-                # for named_stop in named_bus_stops:
-                    # named_stop_name = named_stop[0].strip()
-                    # named_stop_location = getLocation(named_stop_name, all_bus_stops)
-                    # if named_stop_location == this_bus_stop_location:
-                        # timetable = named_stop[1:]
-                        # timetable_chunks = ChunkTimetable(named_stop[1:])
-                        # break
-
                 #Now iterate over all dates
                 thedate = earliest_date
                 # while thedate <= latest_date:
                 while thedate <= earliest_date+datetime.timedelta(days=20):
                     thenextday = thedate + datetime.timedelta(days=1)
-
                     #If it's the wrong day of the week, skip
                     if TimeToScheduleCode(thedate) != schedule_code:
                         thedate = thenextday #Iterate
                         continue
-
                     #Query HDF5 data file
                     starttime = timestamp(thedate, datetime.time(3, 0))
                     endtime = timestamp(thenextday, datetime.time(3, 0))
-
                     queryString = "((route == '%s') & (direction == '%s') & (%f <= time) & (time < %f))" % \
                             (theroute, direction_tag, starttime, endtime)
-
                     trajectories = h5file.root.VehicleLocations.where(queryString)
-
-                    #Calculate spacings
                     spacings, times, vehicle_ids = ExtractArrivalIntervals(trajectories, this_bus_stop_location,
                             doWrite = False)
                     for idx, s in enumerate(spacings):
                         thetime = datetime.datetime.fromtimestamp(times[idx]).time()
                         actual_date_time = datetime.datetime.fromtimestamp(times[idx])
-
-                        # No expected arrival time
-                        # if timetable is None:
-                            # error = numpy.NaN
-                            # expected_unix_time = numpy.NaN
-                        # # Use closest expected arrival time
-                        # else:
-                        # # Finds the closest time.  You need to wrap around if the times span a day
-                            # expected_time = min(timetable, key=
-                                    # lambda time: min([abs(toMicroseconds(time.time())-toMicroseconds(thetime)),
-                                        # 86400000000-abs(toMicroseconds(time.time())-toMicroseconds(thetime))])).time()
-                            # expected_date_time = datetime.datetime.combine(actual_date_time.date(),expected_time)
-                            # error = abs(expected_date_time-actual_date_time).total_seconds()
-                            # # More than twelve hour difference means it was closer the the previous day
-                            # if error>12*60*60:
-                                # new_expected_date_time = expected_date_time + datetime.timedelta(days=1)
-                                # if expected_date_time>actual_date_time:
-                                    # new_expected_date_time = expected_date_time - datetime.timedelta(days=1)
-                                # expected_date_time = new_expected_date_time
-                            # expected_unix_time = unixTimeSeconds(expected_date_time)
-                            # error = abs(expected_date_time-actual_date_time).total_seconds()
-                        # if timetable_chunks is None: #Not a bus stop with timetable data
-                            # chunk_idx = numpy.NaN
-                            # expected_s = numpy.NaN
-                        # else: #Have timetable data
-                            # #Determine which chunk it belongs to
-                            # chunktimes = numpy.array([c[0] for c in timetable_chunks])
-                            # chunk_idx = len(chunktimes[chunktimes < thetime]) - 1
-                            # if chunk_idx < 0:
-                                # chunk_idx += len(chunktimes)
-                            # expected_s = timetable_chunks[chunk_idx][1]
-                        # all_data.append([this_bus_stop_name, stop_idx, vehicle_ids[idx], schedule_code, chunk_idx, times[idx], expected_unix_time, error, s, expected_s])
                         all_data.append([this_bus_stop_name, stop_idx, vehicle_ids[idx], schedule_code, times[idx]])
                     thedate = thenextday #Iterate
+    #Save data
+        data_map = {
+                'stop_name':        [x[0] for x in all_data],
+                'stop_idx':         [x[1] for x in all_data],
+                'vehicle_idx':      [x[2] for x in all_data],
+                'schedule_code':    [x[3] for x in all_data],
+                'time':             [x[4] for x in all_data],
+                }
+        arrival_times = {}
+        for datum in all_data:
+            bus = datum[2]
+            if bus not in arrival_times:
+                arrival_times[bus] = []
+            arrival_times[bus].append(datum)
 
+        for arrival_times_array in arrival_times.values():
+            arrival_times_array.sort(key=lambda x:x[4])
+
+        def get_full_routes(all_arrivals):
+            full_routes = []
+            stop_idxs = [x[1] for x in all_arrivals]
+            first_stop = 6
+            last_stop = 28
+            start_index = 0
+            while first_stop in stop_idxs:
+                i = stop_idxs.index(first_stop)
+                if i+(last_stop-first_stop)<=len(stop_idxs) and stop_idxs[i:i+(last_stop-first_stop)] == range(first_stop,last_stop):
+                    full_routes.append(all_arrivals[i:i+(last_stop-first_stop)])
+                start_index=i+1
+                stop_idxs=stop_idxs[start_index:]
+                all_arrivals=all_arrivals[start_index:]
+            return full_routes
+
+        all_full_routes=[]
+        for bus_arrival_times in arrival_times.values():
+            full_routes = get_full_routes(bus_arrival_times)
+            for route in full_routes:
+                all_full_routes.extend(route)
+
+        full_route_data_map = {
+                'stop_name':        [x[0] for x in all_full_routes],
+                'stop_idx':         [x[1] for x in all_full_routes],
+                'vehicle_idx':      [x[2] for x in all_full_routes],
+                'schedule_code':    [x[3] for x in all_full_routes],
+                'time':             [x[4] for x in all_full_routes],
+                }
+        print(full_route_data_map)
+
+        io.savemat(outfilename, data_map, oned_as = 'row')
+        io.savemat(h5filename[0:-3], full_route_data_map, oned_as = 'row')
         h5file.close()
 
-    #Save data
-    data_map = {
-            'stop_name':        [x[0] for x in all_data],
-            'stop_idx':         [x[1] for x in all_data],
-            'vehicle_idx':      [x[2] for x in all_data],
-            'schedule_code':    [x[3] for x in all_data],
-            'time':             [x[4] for x in all_data],
-            }
-
-    arrival_times = {}
-    for datum in all_data:
-        bus = datum[2]
-        if bus not in arrival_times:
-            arrival_times[bus] = []
-        arrival_times[bus].append(datum)
-
-    for arrival_times_array in arrival_times.values():
-        arrival_times_array.sort(key=lambda x:x[4])
-
-    def get_full_routes(all_arrivals):
-        full_routes = []
-        stop_idxs = [x[1] for x in all_arrivals]
-        first_stop = 6
-        last_stop = 28
-        start_index = 0
-        while first_stop in stop_idxs:
-            i = stop_idxs.index(first_stop)
-            if i+(last_stop-first_stop)<=len(stop_idxs) and stop_idxs[i:i+(last_stop-first_stop)] == range(first_stop,last_stop):
-                full_routes.append(all_arrivals[i:i+(last_stop-first_stop)])
-            start_index=i+1
-            stop_idxs=stop_idxs[start_index:]
-            all_arrivals=all_arrivals[start_index:]
-        return full_routes
-
-    all_full_routes=[]
-    for bus_arrival_times in arrival_times.values():
-        full_routes = get_full_routes(bus_arrival_times)
-        for route in full_routes:
-            all_full_routes.extend(route)
-
-    full_route_data_map = {
-            'stop_name':        [x[0] for x in all_full_routes],
-            'stop_idx':         [x[1] for x in all_full_routes],
-            'vehicle_idx':      [x[2] for x in all_full_routes],
-            'schedule_code':    [x[3] for x in all_full_routes],
-            'time':             [x[4] for x in all_full_routes],
-            }
-
-    io.savemat(outfilename, data_map, oned_as = 'row')
-    io.savemat("/Volumes/Infinity/mbta/out/full_routes", full_route_data_map, oned_as = 'row')
+h5filenames = glob('/Volumes/Infinity/mbta/h5/2013/*.h5')
+processData(h5filenames)
