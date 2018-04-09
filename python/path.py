2ba65bf19a816757ef1cb1d22c3d26eca5c96149
cooper-sloan <cooper@isloan.net>
2018-04-09 13:44:48 -0400

    Beautiful trajectories

--- python/path.py
+++ python/path.py
@@ -1,68 +1,139 @@
 import gmplot
+import tables
+from collections import OrderedDict
 import math
 from scipy import optimize
 import matplotlib.pyplot as plt
 import numpy as np
 
-lats = [42.371048, 42.368632, 42.360855, 42.359038, 42.343444, 42.340393, 42.333535, 42.331213, 42.332889, 42.331979, 42.330677, 42.330037]
-lons = [-71.116074, -71.109294, -71.096016, -71.093586, -71.085840, -71.081463, -71.073351, -71.076999, -71.081076, -71.081699, -71.083244, -71.084317]
+bus_ids = ['0497', '2191', '2194', '2297', '2134', '6005', '2204', '2206', '2229', '2149', '2223', '2220', '6009', '2248', '2151', '2174', '2175', '2245', '2178', '2179', '2266', '2241', '2242', '2243', '2288', '2281', '2283', '2284', '2285', '2286', '2287', '2183', '2186', '2185', '2189', '2260', '2123', '2125', '2124', '2126', '2219', '2235', '2237', '2159', '2147', '2146', '2169', '2142', '2251']
+
+# Paramaterizes the path of the 1 bus
+path_lats = [42.373989, 42.373203, 42.375638, 42.375908, 42.375576, 42.374745, 42.372336, 42.372210, 42.371048, 42.368632, 42.360855, 42.359038, 42.343444, 42.340393, 42.333535, 42.331213, 42.332889, 42.331979, 42.330677, 42.330037]
+path_lons = [-71.118910, -71.118116, -71.118449, -71.117848, -71.115702, -71.114484, -71.115128, -71.115643,-71.116074, -71.109294, -71.096016, -71.093586, -71.085840, -71.081463, -71.073351, -71.076999, -71.081076, -71.081699, -71.083244, -71.084317]
+
 boston_lat = 42.3528
 boston_lon = -71.1048
-n = len(lats)
-# gmap = gmplot.GoogleMapPlotter(boston_lat, boston_lon, 14)
-# gmap.scatter(lats, lons, '#3B0B39', size=40, marker=False)
-# gmap.draw("mymap.html")
+boston_x = 1092798.35753
+boston_y = 2698296.888
+
+n = len(path_lats)
+
+def get_total_distance(xs,ys):
+    d = 0
+    for i in range(len(xs)-1):
+        x1,x2 = xs[i],xs[i+1]
+        y1,y2 = ys[i],ys[i+1]
+        d += np.linalg.norm((x2-x1,y2-y1))
+    return d
+
+def plot_map(lats,lons):
+    gmap = gmplot.GoogleMapPlotter(boston_lat, boston_lon, 14)
+    gmap.scatter(lats, lons, '#3B0B39', size=40, marker=False)
+    gmap.draw("mymap.html")
 
 # Convert to a reasonable x,y paramaterization
 def gps_to_xy(lats,lons):
     # radius of earth, pretty rough though
-    R = 6371* 10
-    xs = [-R*lons[i]*math.cos(lats[0]*180/math.pi) for i in range(len(lats))]
-    # mx = min(xs)
-    # xs = [x-mx for x in xs]
-
-    ys = [R*lats[i] for i in range(len(lats))]
-    # my = min(ys)
-    # ys = [y-my for y in ys]
+    R = 6371
+    # print(R*boston_lat)
+    xs = [-R*lon*math.cos(path_lats[0]*180/math.pi)-boston_y for lon in lons]
+    ys = [R*lat-boston_y for lat in lats]
     return xs,ys
+
+def get_path():
+    return gps_to_xy(path_lats,path_lons)
+
+path = get_path()
+total_distance = get_total_distance(path[0],path[1])
 
 # Takes in a point (x,y) and a set of points (xs,ys) which create a piecewise linear path
 # Then projects the point onto the closest linear section of that path
 def project(x,y,xs,ys):
     smallest_d = "Taylor"
     point = None
+    cum_distance = 0
+    percentage = 0
+    best_proj = 0
     for i in range(len(xs)-1):
         x1,x2 = xs[i],xs[i+1]
         y1,y2 = ys[i],ys[i+1]
-        c = closest_point((x1,y1),(x2,y2),(x,y))
+        c,p = closest_point((x1,y1),(x2,y2),(x,y))
         d = np.linalg.norm(np.subtract(c,(x,y)))
         if d < smallest_d:
             smallest_d = d
+            best_proj = p
+            if p< 0 :
+                p = 0
+            percentage = (cum_distance + p) / total_distance
             point = c
-    return point
+        cum_distance += np.linalg.norm((x2-x1,y2-y1))
+    return point,percentage
 
+# Returns the closest point as well as the distance along the path
 def closest_point(a, b, p):
     a_to_p = np.subtract(p,a)
     a_to_b = np.subtract(b,a)
     proj = np.dot(a_to_p,a_to_b)/np.linalg.norm(a_to_b)
-    return np.add(a,proj*a_to_b/np.linalg.norm(a_to_b))
+    return np.add(a,proj*a_to_b/np.linalg.norm(a_to_b)), proj
 
-samples = [(73.37655118992552, 2783.413447999861), (73.19982539210469, 2783.5599809996784), (73.37655118992552, 2783.413447999861), (73.19982539210469, 2783.5599809996784), (73.19982539210469, 2783.5599809996784), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (58.304365303367376, 2787.395322999917), (4.998815418686718, 2894.173283000011), (6.781221320852637, 2859.4895589998923), (6.781221320852637, 2859.4895589998923), (213.94929995993152, 2854.2334839999676), (4.998815418686718, 2894.173283000011), (4.998815418686718, 2894.173283000011), (162.97148129763082, 2708.5096010002308), (213.94929995993152, 2854.2334839999676), (213.94929995993152, 2854.2334839999676), (162.97148129763082, 2708.5096010002308), (162.97148129763082, 2708.5096010002308), (318.15692888060585, 2578.3755549998023), (142.11278786463663, 2664.5242169997655), (142.11278786463663, 2664.5242169997655), (271.4053814355284, 2601.336638999637), (318.15692888060585, 2578.3755549998023), (318.15692888060585, 2578.3755549998023), (656.8443461414427, 2378.0458309999667)]
+def get_trajectory(filename):
+    route = 1
+    bus = bus_ids[11]
+    direction = '1_1_var0'
+    h5file = tables.open_file(filename)
+    VehicleLocations = h5file.root.VehicleLocations
+    queryString = "((route == '%s') & (direction == '%s'))" % (route, direction)
+    trajectories = VehicleLocations.where(queryString)
+    queryResults = [(timePoint['time'], timePoint['vehicleID'], timePoint['latitude'], timePoint['longitude']) for timePoint in trajectories]
+    queryResults = sorted(queryResults,key=lambda x:x[0])
+    # d = [2] + np.diff([x[0] for x in queryResults])
+    # filtered = [lambda x: d[x[0]] > 2 ,enumerate(queryResults)]
+    filtered = filter(lambda x: x[1] == bus, queryResults)
+    # removes duplicates
+    lats = [x[2] for x in filtered]
+    lons = [x[3] for x in filtered]
+    xs,ys = gps_to_xy(lats,lons)
+    m = min([x[0] for x in filtered])
+    ts = [x[0]-m for x in filtered]
+    return xs,ys,ts
 
-xp = [0,1,1]
-yp = [0,0,-1]
-print(project(0.5,0.5,xp,yp))
-xs,ys = gps_to_xy(lats,lons)
-for p in samples:
-    x,y = p
-    print(project(x,y,xs,ys))
+def get_cuts(xs,ys,ts):
+    cuts = [0]
+    time_threshold = 120
+    distance_threshold = 1000
+    for i in range(len(xs)-1):
+        x1,x2 = xs[i],xs[i+1]
+        y1,y2 = ys[i],ys[i+1]
+        t1,t2 = ts[i],ts[i+1]
+        if t2-t1 > time_threshold:
+            cuts.append(i+1)
+        elif math.sqrt((x2-x1)**2 + (y2-y1)**2) > distance_threshold:
+            cuts.append(i+1)
+    cuts.append(len(xs))
+    return cuts
 
-ps = [project(x,y,xs,ys) for (x,y) in samples]
-for p in ps:
-    print(p)
-print(zip(xs,ys))
-plt.plot(xs, ys, "o")
-# plt.plot([x[0] for x in samples],[x[1] for x in samples], "b")
-plt.axis('equal')
-plt.show()
+def sanitize(percentages,ts):
+    print("\n")
+    print(len(percentages))
+    out = [(percentages[0],ts[0])]
+    for i in range(len(ts)-1):
+        p1 = percentages[i]
+        p2 = percentages[i+1]
+        t1 = ts[i]
+        t2 = ts[i+1]
+        speed = total_distance*(p2-p1)/(t2-t1)
+        # print(speed)
+        if p2 >= out[-1][0]:
+            out.append((p2,t2))
+    print(len(out))
+    return out
+
+
+# xs,ys = gps_to_xy(lats,lons)
+# ps = [project(x,y,xs,ys) for (x,y) in samples]
+# plt.plot(xs, ys, "o")
+# plt.plot([x[0] for x in samples],[x[1] for x in samples], "o")
+# plt.axis('equal')
+# plt.show()
 # plt.show()
--- python/plot_trajectory.py
+++ python/plot_trajectory.py
@@ -1,88 +1,38 @@
-import tables
-import gmplot
 import numpy as np
-import math
 import path
-from scipy.interpolate import splprep, splev
+import pylab
 
 np.set_printoptions(threshold=np.nan)
-filename = "/Volumes/Infinity/mbta/h5/2014/mbta_trajectories_2014_13.h5"
-bus_ids = ['0497', '2191', '2194', '2297', '2134', '6005', '2204', '2206', '2229', '2149', '2223', '2220', '6009', '2248', '2151', '2174', '2175', '2245', '2178', '2179', '2266', '2241', '2242', '2243', '2288', '2281', '2283', '2284', '2285', '2286', '2287', '2183', '2186', '2185', '2189', '2260', '2123', '2125', '2124', '2126', '2219', '2235', '2237', '2159', '2147', '2146', '2169', '2142', '2251']
-route = 1
-bus = bus_ids[11]
-direction = '1_1_var0'
-# h5filenames = glob('/Volumes/Infinity/mbta/h5/2014/*.h5')
-# for filename in h5filenames:
-h5file = tables.open_file(filename)
 
-VehicleLocations = h5file.root.VehicleLocations
-queryString = "((route == '%s') & (direction == '%s'))" % (route, direction)
-trajectories = VehicleLocations.where(queryString)
-
-queryResults = [(timePoint['time'], timePoint['vehicleID'], timePoint['latitude'], timePoint['longitude']) for timePoint in trajectories]
-queryResults = sorted(queryResults,key=lambda x:x[0])
-d = [2] + np.diff([x[0] for x in queryResults])
-# print(d)
-filtered = [lambda x: d[x[0]] > 2 ,enumerate(queryResults)]
-filtered = filter(lambda x: x[1] == bus, queryResults)
-lats = [x[2] for x in filtered]
-lons = [x[3] for x in filtered]
-# radius of earth in m
-xs,ys = path.gps_to_xy(lats,lons)
-m = min([x[0] for x in filtered])
-ts = [x[0]-m for x in filtered]
-
-boston_lat = 42.3528
-boston_lon = -71.1048
-gmap = gmplot.GoogleMapPlotter(boston_lat, boston_lon, 14)
-
-gmap.scatter(lats, lons, '#3B0B39', size=40, marker=False)
-gmap.draw("mymap.html")
-
-
-
-cuts = [0]
-time_threshold = 120
-distance_threshold = 1000
-for i in range(len(xs)-1):
-    x1,x2 = xs[i],xs[i+1]
-    y1,y2 = ys[i],ys[i+1]
-    t1,t2 = ts[i],ts[i+1]
-    if t2-t1 > time_threshold:
-        cuts.append(i+1)
-    elif math.sqrt((x2-x1)**2 + (y2-y1)**2) > distance_threshold:
-        cuts.append(i+1)
-cuts.append(len(xs))
-
-
-# spline parameters
-s=3.0 # smoothness parameter
-k=1 # spline order
-nest=-1 # estimate of number of knots needed (-1 = maximal)
+xs,ys,ts = path.get_trajectory("/Volumes/Infinity/mbta/h5/2014/mbta_trajectories_2014_13.h5")
+cuts = path.get_cuts(xs,ys,ts)
+path_x,path_y = path.get_path()
 
 for i in range(len(cuts)-1):
     cut_start = cuts[i]
     cut_end = cuts[i+1]
     l = cut_end - cut_start
+    # print(l)
     if l < 100:
         continue
-    print("\n")
-    print("length of sequence: ",l)
+
     xss = xs[cut_start:cut_end]
     yss = ys[cut_start:cut_end]
     tss = ts[cut_start:cut_end]
-    print(zip(xss,yss))
-    tckp,u = splprep([xss,yss,tss],s=s,k=k,nest=-1)
-    xnew,ynew,znew = splev(np.linspace(0,1,l*2),tckp)
 
-    import pylab
-    pylab.subplot(1,1,1)
-    data,=pylab.plot(xss,yss,'bo-',label='data')
-    # fit,=pylab.plot(xnew,ynew,'r-',label='fit')
-    for i,v in enumerate(xss):
-        pylab.annotate(i,(xss[i]+np.random.rand()*100,yss[i]+np.random.rand()*100))
-# pylab.scatter(xs,ys)
+    projection = [path.project(x,y,path_x,path_y) for (x,y) in zip(xss,yss)]
+    ps = [p[0] for p in projection]
+    percentages = [p[1] for p in projection]
+    px = [p[0] for p in ps]
+    py = [p[1] for p in ps]
+    sanitary = path.sanitize(percentages, tss)
+    pylab.plot(list(x[1] for x in sanitary),list(x[0] for x in sanitary),'bo-',label='data')
+    # pylab.plot(tss,percentages,'bo-',label='data')
+    # pylab.plot(xss,yss,'bo-',label='data')
+    # print(tss)
+    # pylab.plot(path_x,path_y,'ro-',label='swag')
+    # pylab.plot(px,py,'go-',label='swag')
     pylab.legend()
-
-    pylab.axis('equal')
+    # pylab.axis('equal')
     pylab.show()
+    # break
